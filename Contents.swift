69e29d9021322accf560755145ec0b14a082ae3a
bmorjal <98046359+bmorjal@users.noreply.github.com>
2024-03-10 11:41:45 -0500

    Add files via upload

--- Contents.swift
+++ Contents.swift
@@ -1,16 +1,31 @@
 import UIKit
 
-func fibNumSteps(numSteps: Int) -> [Int]{
-    var sequence = [0, 1]
-    if numSteps <= 1{
-        return sequence
-    }
-    for _ in 0...numSteps - 2{
-        let first = sequence[sequence.count - 2]
-        let second = sequence.last!
-        sequence.append(first + second)
-    }
-    return sequence
-}
+var deskPcItems = ["Msi Monitor", "LG Monitor", 
+                   "Blue Yeti Mic",
+                   "Razor Keyboard", 
+                   "HyperX Headphines",
+                   "Corsair Mouse",
+                   "Logitec Speakers",
+                   "Custom Pc"]
 
-print(fibNumSteps(numSteps: 50))
+var deskPcItemLoc: [String: String] = ["Msi Monitor": "Left Monitor Arm",
+                                       "LG Monitor": "Right Monitor Arm",
+                                       "Blue Yeti Mic": "Right Side Mic Arm",
+                                       "Razor Keyboard": "Middle of desk",
+                                       "HyperX Headphines": "Stand Right Side",
+                                       "Corsair Mouse": "Right of Mouse Pad",
+                                       "Logitec Speakers": "Underneath LG Monitor",
+                                       "Custom Pc": "Right Side of Desk"]
+
+var pcSpecs: [String: String] = ["Cpu": "Ryzen 9 7900x",
+                                 "Gpu": "Asus 3080ti",
+                                 "Board": "Tuff B550",
+                                 "Ram": "Corsair 4x16gb 3600MHZ",
+                                 "Cooler": "Corsair Radiator",
+                                 "Battery": "Corsair 1000watts",
+                                 "Fans": "Corsair x6",
+                                 "Case": "Nzxt"]
+
+print(deskPcItems)
+print(deskPcItemLoc)
+print(pcSpecs)
