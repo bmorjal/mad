c3553ad840a781e5dd03aef36ae84c13d704388a
bmorjal <98046359+bmorjal@users.noreply.github.com>
2024-03-10 12:41:34 -0500

    Add files via upload

--- Contents.swift
+++ Contents.swift
@@ -1,31 +1,27 @@
 import UIKit
 
-var deskPcItems = ["Msi Monitor", "LG Monitor", 
-                   "Blue Yeti Mic",
-                   "Razor Keyboard", 
-                   "HyperX Headphines",
-                   "Corsair Mouse",
-                   "Logitec Speakers",
-                   "Custom Pc"]
+func gasCalc(){
+    var timePrices = [1.3, 1.1, 1.6, 1.8, 1.4, 1.7, 1.4, 2.3]
+    var yearPrices = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]
+    
+    var sortPrices = timePrices.sorted{
+        (firstValue, secondValue) -> Bool in
+        return firstValue < secondValue
+    }
+    
+    let price = 1.7
+    let price2 = 1.4
+    let price3 = 2.3
+    
+    var add = (price + price2 + price3)
+    
+    var calc = (add / 3)
+    
+    print("The average is for the three years is", calc)
+    
+    print(sortPrices)
+}
 
-var deskPcItemLoc: [String: String] = ["Msi Monitor": "Left Monitor Arm",
-                                       "LG Monitor": "Right Monitor Arm",
-                                       "Blue Yeti Mic": "Right Side Mic Arm",
-                                       "Razor Keyboard": "Middle of desk",
-                                       "HyperX Headphines": "Stand Right Side",
-                                       "Corsair Mouse": "Right of Mouse Pad",
-                                       "Logitec Speakers": "Underneath LG Monitor",
-                                       "Custom Pc": "Right Side of Desk"]
+gasCalc()
 
-var pcSpecs: [String: String] = ["Cpu": "Ryzen 9 7900x",
-                                 "Gpu": "Asus 3080ti",
-                                 "Board": "Tuff B550",
-                                 "Ram": "Corsair 4x16gb 3600MHZ",
-                                 "Cooler": "Corsair Radiator",
-                                 "Battery": "Corsair 1000watts",
-                                 "Fans": "Corsair x6",
-                                 "Case": "Nzxt"]
 
-print(deskPcItems)
-print(deskPcItemLoc)
-print(pcSpecs)
